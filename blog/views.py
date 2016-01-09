10d17c23d874ab7c947cae47e4c35930aa672d2f
myrh000 <dominicyale@126.com>
2016-01-09 17:42:26 +0800

    20160109

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader, Context
+
+from blog.models import BlogPost
+
+def archive(request):
+    posts = BlogPost.objects.all()
+    t = loader.get_template("archive.html")
+    c = Context({ 'posts': posts })
+    return HttpResponse(t.render(c))
